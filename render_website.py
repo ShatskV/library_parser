ac28ed09274032c06a847d7c6cd9a2ea876848bf
Vladimir Shatskiy <vovaysach@gmail.com>
2022-07-04 23:50:37 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,31 +1,49 @@
 import json
+import os
+import shutil
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+BOOKS_PER_PAGE = 10
 
-template = env.get_template('template.html')
 
-with open('books.json', 'r') as file:
-    books = json.load(file)
+def render_pages(template, books, folder='pages', num_books_per_page=10):
+    if os.path.exists(folder) and os.path.isdir(folder):
+        shutil.rmtree(folder)
+    os.makedirs(folder, exist_ok=True)
+    books_per_pages = list(chunked(books, num_books_per_page))
+    filename_template = 'index{}.html'
+    for num, books_per_page in enumerate(books_per_pages):
+        books_chunked = list(chunked(books_per_page, 2))
+        filepath = os.path.join(folder, filename_template.format(num))
+        rendered_page = template.render(books=books_chunked)
+        with open(filepath, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
-books_chunked = list(chunked(books, 2))
+        
 def on_reload():
-    rendered_page = template.render(books=books_chunked)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    env = Environment(
+            loader=FileSystemLoader('.'),
+            autoescape=select_autoescape(['html', 'xml'])
+            )
+    template = env.get_template('template.html')
+    with open('books.json', 'r') as file:
+        books = json.load(file)
+    render_pages(template, books)
     print("Site rebuild")
 
 
-on_reload()
+def main():
+    on_reload()
 
-server = Server()
+    server = Server()
 
-server.watch('template.html', on_reload)
+    server.watch('template.html', on_reload)
 
-server.serve(root='.')
+    server.serve(root='.')
+
+
+if __name__ == "__main__":
+    main()
